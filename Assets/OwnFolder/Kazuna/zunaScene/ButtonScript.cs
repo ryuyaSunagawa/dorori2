8f3b4c48d77e57d507972342241986ef26d6f9a3
iam8bit <11807121@stu.kbc.ac.jp>
2020-06-06 14:20:41 +0900

    tekito

--- Assets/OwnFolder/Kazuna/zunaScene/ButtonScript.cs
+++ Assets/OwnFolder/Kazuna/zunaScene/ButtonScript.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {  
-    public void OnClickButton()
+    public void OnClickStart()
     {
         SceneManager.LoadScene("FixScene");
     }
-
+    public void OnClickEnd()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEngine.Application.Quit();
+    }
 }
