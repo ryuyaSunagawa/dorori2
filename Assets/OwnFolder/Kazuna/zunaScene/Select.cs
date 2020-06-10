d20d00f56be1b4457738b58a9596f23331beb517
iam8bit <11807121@stu.kbc.ac.jp>
2020-06-10 10:17:09 +0900

    Mozambique

--- Assets/OwnFolder/Kazuna/zunaScene/Select.cs
+++ Assets/OwnFolder/Kazuna/zunaScene/Select.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Select : MonoBehaviour
 {
+    [SerializeField] EventSystem eventSystem;
+
+    GameObject selectObj;
+
     // Start is called before the first frame update
     void Start()
     {
         Selectable sel = GetComponent<Selectable>();
         sel.Select();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space)){
+            selectObj = eventSystem.currentSelectedGameObject.gameObject;
+            if (selectObj.name == "StartButton")
+            {
+                ButtonScript s = GetComponent<ButtonScript>();
+                s.OnClickStart();
+            }
+            if (selectObj.name == "EndButton")
+            {
+                ButtonScript s = GetComponent<ButtonScript>();
+                s.OnClickEnd();
+            }
+        }
     }
 }
