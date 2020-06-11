434ca3cacf1cbe958cea8f58e5fb1f2133742168
ryuyaSunagawa <11806134@stu.kbc.ac.jp>
2020-06-11 15:15:52 +0900

    ビルドアップデート バグ修正・カメラ修正

--- Assets/OwnFolder/Nohina/FixProject/Scripts/CameraController.cs
+++ Assets/OwnFolder/Nohina/FixProject/Scripts/CameraController.cs
@@ -8,11 +8,19 @@ public class CameraController : MonoBehaviour
 	/// カメラがターゲッティングするオブジェクト
 	/// </summary>
 	[SerializeField] Transform targetObject;
+	[SerializeField] Transform enemyObject;
+
+	[SerializeField, Range( 0f, 1f ), Header( "プレイヤーの横ずれ" ) ] float xDistance = 0.5f;
+
+	[SerializeField, Range( 0f, 1f ), Header( "プレイヤーの縦wずれ" ) ] float yDistance = 0.5f;
+
+	[SerializeField, Header( "敵と自分の中間点にカメラを置くか" ) ] bool chaseAllMen = true;
+	
 
 	/// <summary>
 	/// 遅れる速度(かな?)
 	/// </summary>
-	[SerializeField, Range( 0f, 0.5f)] float SmoothDegree = 0.2f;
+	[SerializeField, Range( 0f, 0.5f), Header( "カメラの遅延量" ) ] float smoothDegree = 0.2f;
 
 	/// <summary>
 	/// 移動速度
@@ -30,17 +38,34 @@ public class CameraController : MonoBehaviour
     void Start()
     {
 		myCamera = GetComponent<Camera>();
-		defaultViewportPosition = myCamera.WorldToViewportPoint( targetObject.position );
+		//defaultViewportPosition = myCamera.WorldToViewportPoint( targetObject.position );
 	}
 
     // Update is called once per frame
     void FixedUpdate()
     {
 		Vector3 myPosition = transform.position;
-		Vector3 targetViewportPoint = myCamera.WorldToViewportPoint( targetObject.position );
-		Vector3 delta = targetObject.position - myCamera.ViewportToWorldPoint( new Vector3( defaultViewportPosition.x, defaultViewportPosition.y, targetViewportPoint.z ) );
+		Vector3 realTargettingPosition = targetObject.position;
+
+		Vector3 cameraRightEndPosition = myCamera.ViewportToWorldPoint( new Vector3( 1.0f, 0.0f, 0.0f ) );
+
+		if( enemyObject )
+		{
+			Vector3 ahan = enemyObject.position - cameraRightEndPosition;
+			Debug.Log( ahan );
+
+			if( ahan.x < 1 && ahan.x > -38 && chaseAllMen )
+			{
+				realTargettingPosition = Vector3.Lerp( enemyObject.position, targetObject.position, 0.5f );
+
+				realTargettingPosition.z = -10;
+			}
+		}
+
+		Vector3 targetViewportPoint = myCamera.WorldToViewportPoint( realTargettingPosition );
+		Vector3 delta = realTargettingPosition - myCamera.ViewportToWorldPoint( new Vector3( xDistance, yDistance, targetViewportPoint.z ) );
 		Vector3 destination = myPosition + delta;
 
-		transform.position = Vector3.SmoothDamp( myPosition, destination, ref moveVelocity, SmoothDegree );
+		transform.position = Vector3.SmoothDamp( myPosition, destination, ref moveVelocity, smoothDegree );
 	}
 }
