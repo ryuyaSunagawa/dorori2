69bfcca11463c5d3da3ac1011e81bf4835ce712e
iam8bit <11807121@stu.kbc.ac.jp>
2020-06-12 17:04:47 +0900

    円形ゲージとか作ってみたけど使わないなら構わない 長押しとかで使えたらいいな程度

---
+++ Assets/OwnFolder/Kazuna/zunaScene/gage/GageScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GageScript : MonoBehaviour
+{
+    Image gage;
+    float meter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meter += 0.001f;
+        gage.fillAmount = meter;
+        if (meter >= 1) meter = 0;
+    }
+}
